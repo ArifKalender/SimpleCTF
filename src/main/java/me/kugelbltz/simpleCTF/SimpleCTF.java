83b5ca0bbc019550acff172c5ab49d45e169e29f
Kugelbltz <arifkalndr@gmail.com>
2026-03-19 20:40:39 +0300

    Initial commit

---
+++ src/main/java/me/kugelbltz/simpleCTF/SimpleCTF.java
@@ -0,0 +1,17 @@
+package me.kugelbltz.simpleCTF;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleCTF extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
