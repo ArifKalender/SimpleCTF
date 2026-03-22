1085bd6b0d3312b4d9c48ffba081507beba36802
Kugelbltz <arifkalndr@gmail.com>
2026-03-23 01:20:04 +0300

    Switch from legacy string handling for names to MiniMessage for items

--- src/main/java/me/kugelbltz/simpleCTF/model/BannerItems.java
+++ src/main/java/me/kugelbltz/simpleCTF/model/BannerItems.java
@@ -1,8 +1,11 @@
 package me.kugelbltz.simpleCTF.model;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import static me.kugelbltz.simpleCTF.SimpleCTF.getMM;
 
 public class BannerItems {
     public final ItemStack blueFlag = new ItemStack(Material.BLUE_BANNER, 1);
@@ -10,9 +13,9 @@ public class BannerItems {
 
     public BannerItems() {
         ItemMeta redMeta = redFlag.getItemMeta();
-        redMeta.setDisplayName("§cRed Flag");
+        redMeta.displayName(getMM().deserialize("<red>Red Flag"));
         ItemMeta blueMeta = blueFlag.getItemMeta();
-        blueMeta.setDisplayName("§9Blue Flag");
+        blueMeta.displayName(getMM().deserialize("<blue>Blue Flag"));
 
         redFlag.setItemMeta(redMeta);
         blueFlag.setItemMeta(blueMeta);
@@ -20,14 +23,16 @@ public class BannerItems {
 
     public boolean isRedFlag(ItemStack itemStack) {
         if (itemStack.getItemMeta() == null) return false;
-        String displayName = itemStack.getItemMeta().getDisplayName();
-        return itemStack.getType() == Material.RED_BANNER && displayName.equals(this.redFlag.getItemMeta().getDisplayName());
+        Component displayName = itemStack.getItemMeta().displayName();
+        if (displayName == null) return false;
+        return itemStack.getType() == Material.RED_BANNER && displayName.equals(this.redFlag.getItemMeta().displayName());
     }
 
     public boolean isBlueFlag(ItemStack itemStack) {
         if (itemStack.getItemMeta() == null) return false;
-        String displayName = itemStack.getItemMeta().getDisplayName();
-        return itemStack.getType() == Material.BLUE_BANNER && displayName.equals(this.blueFlag.getItemMeta().getDisplayName());
+        Component displayName = itemStack.getItemMeta().displayName();
+        if (displayName == null) return false;
+        return itemStack.getType() == Material.BLUE_BANNER && displayName.equals(this.blueFlag.getItemMeta().displayName());
     }
 
     public boolean isFlag(ItemStack itemStack) {
