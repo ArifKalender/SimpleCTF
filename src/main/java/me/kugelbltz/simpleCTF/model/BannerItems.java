f961a67339d8d63c927585da6d5302c81ab255f5
Kugelbltz <arifkalndr@gmail.com>
2026-03-24 21:21:21 +0300

    Enhance item handling

--- src/main/java/me/kugelbltz/simpleCTF/model/BannerItems.java
+++ src/main/java/me/kugelbltz/simpleCTF/model/BannerItems.java
@@ -1,8 +1,10 @@
 package me.kugelbltz.simpleCTF.model;
 
-import net.kyori.adventure.text.Component;
+import me.kugelbltz.simpleCTF.SimpleCTF;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 import static me.kugelbltz.simpleCTF.SimpleCTF.getBannerItems;
 import static me.kugelbltz.simpleCTF.SimpleCTF.getMM;
@@ -10,11 +12,14 @@ import static me.kugelbltz.simpleCTF.SimpleCTF.getMM;
 public class BannerItems {
     private final ItemStack blueFlag = new ItemStack(Team.BLUE.getBannerItem(), 1);
     private final ItemStack redFlag = new ItemStack(Team.RED.getBannerItem(), 1);
+    NamespacedKey key = new NamespacedKey(SimpleCTF.getInstance(), "team_flag");
 
     public BannerItems() {
         ItemMeta redMeta = redFlag.getItemMeta();
+        redMeta.getPersistentDataContainer().set(key, PersistentDataType.STRING, "RED");
         redMeta.displayName(getMM().deserialize("<red>Red Flag"));
         ItemMeta blueMeta = blueFlag.getItemMeta();
+        blueMeta.getPersistentDataContainer().set(key, PersistentDataType.STRING, "BLUE");
         blueMeta.displayName(getMM().deserialize("<blue>Blue Flag"));
 
         redFlag.setItemMeta(redMeta);
@@ -32,15 +37,16 @@ public class BannerItems {
 
     /** @return whether the given ItemStack belongs to any team */
     public boolean isFlag(ItemStack itemStack) {
-        return isFlag(itemStack, Team.RED) || isFlag(itemStack, Team.BLUE);
+        return itemStack.getPersistentDataContainer().has(key);
     }
 
     /** @return whether the given ItemStack belongs to the given Team */
     public boolean isFlag(ItemStack itemStack, Team team) {
         if (itemStack.getItemMeta() == null) return false;
-        Component displayName = itemStack.displayName();
-        if (team == Team.BLUE && displayName.equals(this.blueFlag.displayName())) return true;
-        if (team == Team.RED && displayName.equals(this.redFlag.displayName())) return true;
+        String itemTeam = itemStack.getItemMeta().getPersistentDataContainer().get(key, PersistentDataType.STRING);
+        if (itemTeam == null) return false;
+        if (itemTeam.equals("RED") && team == Team.RED) return true;
+        if (itemTeam.equals("BLUE") && team == Team.BLUE) return true;
         else return false;
     }
 
